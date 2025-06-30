b8ee239ebe328d9aa429fc3087877efb74afb7b4
PraveenWE <praveentecsl@gmail.com>
2025-06-30 23:15:03 +0530

    initialize put & delete

--- backend/index.js
+++ backend/index.js
@@ -15,4 +15,17 @@ app.post("/api/user", (req, res) => {
   res.json({ greetin: `hello ,${name} praveen chandeepa ` });
 });
 
+app.put("/api/user/:id", (req, res) => {
+  const userId = req.params.id;
+  const { name } = req.body;
+
+  res.json({ massage: `user ${userId} is updated to ${name}` });
+});
+
+app.delete("/api/user/:id", (req, res) => {
+  const userId = req.params.id;
+
+  res.json({ information: `your user_id ${userId} is delelted successfully` });
+});
+
 app.listen(port, () => console.log("Backend is running on port 5000"));
