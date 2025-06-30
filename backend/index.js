a70213fec00e348030fe9470e946d7bce58591ab
PraveenWE <praveentecsl@gmail.com>
2025-06-30 22:43:28 +0530

    add get and post req for the system

--- backend/index.js
+++ backend/index.js
@@ -6,4 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/hello", (req, res) => {
+  res.json({ massage: "hello from express and praveen" });
+});
+
+app.post("/api/user", (req, res) => {
+  const { name } = req.body;
+  res.json({ greetin: `hello ,${name} praveen chandeepa ` });
+});
+
 app.listen(port, () => console.log("Backend is running on port 5000"));
